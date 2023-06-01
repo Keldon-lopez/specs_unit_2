a40058c7bb840ae6a94fe0c73593b8f7894e0a77
Keldon Lopez <keldon.lopz@gmail.com>
2023-06-01 12:01:43 -0600

    started part 2

--- movie-watchlist/src/App.js
+++ movie-watchlist/src/App.js
@@ -1,24 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+
+
+
 
 function App() {
+  const [ movieList,setMovieList ] = useState([]);
+  const [watchList, setWatchlist] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const getData = () => {
+    axios
+        .get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=${page}`)
+        .then((res) => {
+            console.log(res.data.results);
+            setMovieList(res.data.results);
+        });
+  }
+
+  useEffect(() => {
+    getData();
+}, [page]);
+
   return (
     <div className="App">
       <header className="App-header">
         <Header/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
